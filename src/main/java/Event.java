0979d60b3d5348d89c8cae942500be196cb24418
Janek Brandt <brandt.janek@gmail.com>
2017-03-17 08:38:52 -0700

    add test to ensure new event is event object and that number of guests is set correctly

--- src/main/java/Event.java
+++ src/main/java/Event.java
@@ -1,3 +1,11 @@
 public class Event {
-  
+  private int mGuestCount;
+
+  public Event(int guestCount) {
+    mGuestCount = guestCount;
+  }
+
+  public int getGuestCount() {
+    return mGuestCount;
+  }
 }
--- src/test/java/EventTest.java
+++ src/test/java/EventTest.java
@@ -2,5 +2,17 @@ import org.junit.*;
 import static org.junit.Assert.*;
 
 public class EventTest {
-  
+
+
+  @Test
+  public void newEvent_instantiatedCorrectly() {
+    Event testEvent = new Event(100);
+    assertEquals(true, testEvent instanceof Event);
+  }
+
+  @Test
+  public void getGuestCount_returnsTheCorrectNumberOfGuests_100() {
+    Event testEvent = new Event(100);
+    assertEquals(100, testEvent.getGuestCount());
+  }
 }
